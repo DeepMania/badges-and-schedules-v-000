77f1dbaf54da2d79f186cc059170b717b3b9d1f5
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-03 01:10:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(speakers)
+ batch_badges_printed = []
+ speakers.each do |name|
+ batch_badges_printed << "Hello, my name is" +" "+" #{name}."
+end
+
+end
