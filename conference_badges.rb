67104e0fdd444276169e0213922ba97779666666
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-03 02:09:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ room_assignments = []
   attendees.each_with_index do |name, index|
     room_assignments << "Hello, "+"#{name}!"+" You'll be assigned to room "+"#{index+1}!"
   end
- return assign_rooms
+ return room_assignments
  end
 
 def printer(badges, room_assignments)
