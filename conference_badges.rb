c0d10bf34bf64da34e562e7b7a9f1885006d34c8
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-03 01:16:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,6 @@ def batch_badge_creator(speakers)
  batch_badges_printed = []
  speakers.each do |name|
  batch_badges_printed << "Hello, my name is"+" #{name}."
-
 end
 return batch_badges_printed
 end
